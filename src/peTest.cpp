c3bba7d78ce6e092927368a6628753bfd9ff483e
louli <louli@xiimoon.com>
2022-08-11 10:28:47 +0800

    路径调整

--- src/peTest.cpp
+++ src/peTest.cpp
@@ -1,8 +1,17 @@
 #include "peTool.h"
 
 namespace peTest {
+	const char* p1 = "E:/code/cpp/simple-cpp/res/";
+	const char* p2 = "D:/Code/vsDir/simple-cpp/res/";
+	const char* FILE_ROOT = p2;
+
+
 	void test1() {
-		copyBin("E:/code/cpp/simple-cpp/res/notepad.exe", "E:/code/cpp/simple-cpp/res/copyBin.exe");
+		char srcPath[50];
+		sprintf(srcPath, "%s%s", FILE_ROOT, "notepad.exe");
+		char dstPath[50];
+		sprintf(dstPath, "%s%s", FILE_ROOT, "copyBin.exe");
+		copyBin(srcPath, dstPath);
 		log("finish");
 	}
 }
